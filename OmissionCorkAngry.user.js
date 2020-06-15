58c16f0366175cf0acc55623f74fa5ab2a8a99a7
ScruffyRules <5356779+ScruffyRules@users.noreply.github.com>
2020-06-16 03:00:06 +0930

    oh and y'know, bump the version

--- OmissionCorkAngry.user.js
+++ OmissionCorkAngry.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VRChat Site Enhanced
 // @namespace    ScruffyRules
-// @version      0.02
+// @version      0.03
 // @description  Trying to enchance VRChat's website with extra goodies
 // @author       ScruffyRules
 // @match        https://vrchat.com/home/*
