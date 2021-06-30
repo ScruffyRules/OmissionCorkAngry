fbfff3d848a59c008402a4f7d2f7b343608395a5
ScruffyRules <5356779+ScruffyRules@users.noreply.github.com>
2021-06-30 18:01:10 +0930

    Actually change ver number

--- OmissionCorkAngry.user.js
+++ OmissionCorkAngry.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VRChat Site Enhanced
 // @namespace    ScruffyRules
-// @version      0.107
+// @version      0.108
 // @description  Trying to enchance VRChat's website with extra goodies
 // @author       ScruffyRules
 // @match        https://vrchat.com/home/*
